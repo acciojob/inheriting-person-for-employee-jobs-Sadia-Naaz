a5971605c795bbaa643985a0f31e2a57856cdac6
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2024-01-15 19:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+greet(){
+	console.log("hello my name is ${name},i am ${age} years old.")
+}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name=name;
+	this.age=age;
+	this.jobTitle=jobTitle;
+	__prototype__:Person(name,age);
+	jobGreet()
+	{
+		console.log("hello my name is ${name},i am ${age} years old ,and  my job title is ${jobTitle}");
+	}
+}
 
 // Do not change code below this line
+
 window.Person = Person;
 window.Employee = Employee;
